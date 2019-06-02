617e1bbc3101334ea42ab24a19b077d6da8253b1
Koot11 <lol997koot11>
2019-06-02 23:43:57 +0200

    Added denoising autoencoder for mnist dataset

--- data/data_loading/load_mnist.py
+++ data/data_loading/load_mnist.py
@@ -2,7 +2,7 @@ from keras.datasets import mnist
 from keras.utils import to_categorical
 
 
-def load_mnist_flat():
+def load_mnist_flat(normalize=None):
     (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
     x_train = x_train.reshape(x_train.shape[0], x_train.shape[1]*x_train.shape[2])
@@ -10,10 +10,13 @@ def load_mnist_flat():
     y_train = to_categorical(y_train, 10)
     y_test = to_categorical(y_test, 10)
 
+    if normalize is not None:
+        x_train, x_test = normalization(x_train, x_test, normalize)
+
     return (x_train, y_train), (x_test, y_test)
 
 
-def load_mnist_2d_arr(channels_first=False):
+def load_mnist_2d_arr(channels_first=False, normalize=None):
     (x_train, y_train), (x_test, y_test) = mnist.load_data()
     if channels_first:
         x_train = x_train.reshape(x_train.shape[0], 1, x_train.shape[1], x_train.shape[2])
@@ -24,7 +27,25 @@ def load_mnist_2d_arr(channels_first=False):
     y_train = to_categorical(y_train, 10)
     y_test = to_categorical(y_test, 10)
 
+    if normalize is not None:
+        x_train, x_test = normalization(x_train, x_test, normalize)
+        
     return (x_train, y_train), (x_test, y_test)
+
+
+def normalization(x_train, x_test, normalize):
+    if normalize == -1:
+        x_train /= 122.5
+        x_test /= 122.5
+
+        x_train -= 1
+        x_test -= 1
+
+    elif normalize == 0:
+        x_train /= 255
+        x_test /= 255
+
+    return x_train, x_test
 
 
 # End of file
--- neural_networks/models/direct_models/denoising_autoencoder.py
+++ neural_networks/models/direct_models/denoising_autoencoder.py
@@ -1,45 +1,48 @@
 from keras.models import Model
-from keras.layers import Input, Dense, Conv2D, MaxPool2D, Flatten, UpSampling2D
+from keras.layers import Input, Dense, Conv2D, MaxPool2D, Flatten, UpSampling2D, Conv2DTranspose
 from keras.optimizers import SGD
 from data.data_loading.load_mnist import load_mnist_2d_arr
 import numpy as np
 import cv2
 
 
-def get_model(input_width=28, input_height=28, input_depth=1, learning_rate=0.001):
+def get_model(input_width=28, input_height=28, input_depth=1, learning_rate=0.001, momentum=0.8):
 
     input_layer = Input(shape=(input_width, input_height, input_depth,))
 
-    x = Conv2D(16, (3, 3), activation='relu', padding='same')(input_layer)
+    x = Conv2D(8, (7, 7), activation='relu', padding='same')(input_layer)
     x = MaxPool2D((2, 2), padding='same')(x)
-    x = Conv2D(8, (3, 3), activation='relu', padding='same')(x)
-    x = MaxPool2D((2, 2), padding='same')(x)
-    x = Conv2D(8, (3, 3), activation='relu', padding='same')(x)
+    x = Conv2D(16, (5, 5), activation='relu', padding='same')(x)
+    x = Conv2D(32, (3, 3), activation='relu', padding='same')(x)
     encoded = MaxPool2D((2, 2), padding='same')(x)
 
     # at this point the representation is (4, 4, 8) i.e. 128-dimensional
 
-    x = Conv2D(8, (3, 3), activation='relu', padding='same')(encoded)
+    x = Conv2DTranspose(32, (3, 3), activation='relu', padding='same')(encoded)
     x = UpSampling2D((2, 2))(x)
-    x = Conv2D(8, (3, 3), activation='relu', padding='same')(x)
+    x = Conv2DTranspose(16, (5, 5), activation='relu', padding='same')(x)
+    x = Conv2DTranspose(8, (7, 7), activation='relu', padding='same')(x)
     x = UpSampling2D((2, 2))(x)
-    x = Conv2D(16, (3, 3), activation='relu')(x)
-    x = UpSampling2D((2, 2))(x)
-    decoded = Conv2D(1, (3, 3), activation='sigmoid', padding='same')(x)
+    decoded = Conv2DTranspose(1, (3, 3), activation='sigmoid', padding='same')(x)
 
     model = Model(inputs=input_layer, outputs=decoded)
-    optimizer = SGD(lr=learning_rate)
+    optimizer = SGD(lr=learning_rate, momentum=momentum, decay=0)
 
-    model.compile(optimizer='adadelta', loss='mae', metrics=['mse'])
+    model.compile(optimizer=optimizer, loss='binary_crossentropy')
     return model
 
 
 def main():
     (y_train, _), (y_test, _) = load_mnist_2d_arr(False)
-    train_noise = np.random.normal(125.5, 50, y_train.shape)
-    test_noise = np.random.normal(125.5, 50, y_test.shape)
-    x_train = y_train + train_noise
-    x_test = y_test + test_noise
+    y_train = y_train / 255
+    y_test = y_test / 255
+    noise_scale = 0.5
+    train_noise = np.random.normal(0, 1, y_train.shape)
+    test_noise = np.random.normal(0, 1, y_test.shape)
+    x_train = y_train + noise_scale * train_noise
+    x_test = y_test + noise_scale * test_noise
+    x_test = np.clip(x_test, .0, 1.0)
+    x_train = np.clip(x_train, .0, 1.0)
     print(x_train.shape)
     model = get_model(learning_rate=0.1)
     model.summary()
@@ -50,8 +53,8 @@ def main():
     mnist_evalset_input_path = "C:\\cv_portfolio\\simple_nn_framework\\data\\datasets\\mnist_evalset_input\\"
     mnist_evalset_output_path = "C:\\cv_portfolio\\simple_nn_framework\\data\\datasets\\mnist_evalset_output\\"
     for i in range(res.shape[0]//10):
-        cv2.imwrite(mnist_evalset_input_path + str(i) + ".png", x_test[i])
-        cv2.imwrite(mnist_evalset_output_path + str(i) + ".png", x_test[i])
+        cv2.imwrite(mnist_evalset_input_path + str(i) + ".png", x_test[i]*255)
+        cv2.imwrite(mnist_evalset_output_path + str(i) + ".png", res[i]*255)
 
 
 if __name__ == '__main__':
